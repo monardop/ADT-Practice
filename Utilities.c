106560c8e4627956f25f691a4442336db1b68422
Pablo Monardo <pablomonardo@gmail.com>
2022-06-02 09:22:37 -0300

    Defined functions that I'm going to use and two structs

--- Utilities.c
+++ Utilities.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Utilities.h"
 
+
+void memcpy(void* dst, const void* src, const size_t num) {
+    //typecast src and dst to char:
+    char* cSrc = (char*) src;
+    char* cDst = (char*) dst;
+
+    //copy contents of src[] to dst[]
+    for(int i =0; i<num; i++) 
+        cDst[i] = cSrc[i];
+}
+int lenght(const void* element) {
+    char* tElement = (char*) element;
+    int i = 0;
+    while (tElement != '\0') {
+        i++;
+    }
+    return i;
+}
--- Utilities.h
+++ Utilities.h
@@ -1,1 +1,17 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
 
+typedef struct Node
+{
+    void* data;
+    size_t dataSize;
+    struct Node* next;
+}tNode;
+typedef enum bool {True, False};
+
+void memcpy(void*, const void*, const size_t);
+int lenght(const void*);
+
+
+
+#endif
